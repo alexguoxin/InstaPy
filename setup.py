f1413ce2ed3a833fce3973ebeb3c12aa0ba19e6e
Alex <alex.guoxin@gmail.com>
2018-03-16 17:20:21 -0700

    fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='InstaPy',
     version='0.1',
-    packages=['InstaPy'],
+    packages=['instapy'],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
