2ed74263c97e8003ff8db133185ed8fd9641f595
Alex <alex.guoxin@gmail.com>
2018-03-16 22:12:12 -0700

    fix greyscale test

--- instapy/test/test_greyscale.py
+++ instapy/test/test_greyscale.py
@@ -42,7 +42,7 @@ def test_greyscale1():
     greyscale("instapy/test/test_img/greyscale/test_img1.jpg",
               "instapy/test/test_img/greyscale/test_img1_gs.jpg")
     output = plt.imread("instapy/test/test_img/greyscale/test_img1_gs.jpg")[:, :, :3]
-    assert (output == test_img1_gs).all(), "The greyscale function does not work properly."
+    assert np.array_equal(output, test_img1_gs), "The greyscale function does not work properly."
 
 # test_img2: greyscale image
 test_img2 = np.array([[[ 19,  19,  19], [ 31,  31,  31], [ 20,  20,  20]],
@@ -56,9 +56,9 @@ def test_greyscale2():
     greyscale("instapy/test/test_img/greyscale/test_img2.jpg",
               "instapy/test/test_img/greyscale/test_img2_gs.jpg")
     output = plt.imread("instapy/test/test_img/greyscale/test_img2_gs.jpg")[:, :, :3]
-    assert (output == test_img2).all(), "The greyscale function should not change a greyscale image."
+    assert np.array_equal(output, test_img2), "The greyscale function should not change a greyscale image."
 
-# test of exception handling
+# test for exception handling
 def test_non_string_input():
     with pytest.raises(TypeError):
         greyscale(123, "instapy/test/test_img/greyscale/test_img1_gs.jpg")
